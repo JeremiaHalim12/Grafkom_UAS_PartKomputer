b005101e90282fea2a5b912998eda0eda631aba9
JeremiaHalim <2072021@maranatha.ac.id>
2024-01-11 03:10:52 +0700

    update comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
-// scene, camera, renderer
+// SCENE, CAMERA, RENDERER
 const scene = new THREE.Scene();
 const cam = new THREE.PerspectiveCamera(
     90,
@@ -16,7 +16,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.BasicShadowMap
 
-// cam position
+// CAM POSITION
 // cam.position.x = 0;
 cam.position.y = 17;
 cam.position.z = 22;
@@ -24,7 +24,7 @@ cam.position.z = 22;
 document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(cam, renderer.domElement);
 
-
+// 3D MODEL
 // KEYBOARD
 const keyboardLoader = new GLTFLoader();
 keyboardLoader.load('asset/keyboard/scene.gltf', (gltf) => {
@@ -80,6 +80,7 @@ tableLoader.load('asset/table/scene.gltf', (gltf) => {
     scene.add(gltf.scene);
 });
 
+// PLANE
 const plane = new THREE.PlaneGeometry( 1000,1000,500,500 );
 const planeMaterial = new THREE.MeshLambertMaterial( { color: 0xffffff });
 const planeMesh = new THREE.Mesh( plane, planeMaterial );
@@ -88,6 +89,8 @@ planeMesh.position.set(-10,-1,0);
 planeMesh.rotation.x = -Math.PI/2;
 scene.add( planeMesh );
 
+
+// LIGHTING
 var ambient = new THREE.AmbientLight( 0xffffff, 0.02);
 scene.add( ambient );
 
@@ -112,6 +115,8 @@ spotlight.target.position.set(3,1,0);
 spotlight.castShadow = true;
 scene.add(spotlight);
 
+
+// SHOW MODEL
 function animate() {
     requestAnimationFrame(animate);
     controls.update();
