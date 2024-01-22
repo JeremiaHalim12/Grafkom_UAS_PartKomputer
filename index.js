9ee79895efea2aa6a055df86a7655d9d0073e133
JeremiaHalim <2072021@maranatha.ac.id>
2024-01-23 01:43:41 +0700

    Fix Raycast
    fix raycasting pada PointerLock (pointer merah), fix modelLoader function jadi raycast sederhana

--- index.js
+++ index.js
@@ -1,115 +1,79 @@
 import * as THREE from "three";
 import { OrbitControls } from "./node_modules/three/examples/jsm/controls/OrbitControls.js";
 import { PointerLockControls } from "./node_modules/three/examples/jsm/controls/PointerLockControls.js";
-import * as lib from './model_loader.js';
+import * as lib from "./model_loader.js";
 
 // SCENE, CAMERA, RENDERER
 const scene = new THREE.Scene();
 const cam = new THREE.PerspectiveCamera(
-    90,
-    window.innerWidth / window.innerHeight,
-    1,
-    100
+  90,
+  window.innerWidth / window.innerHeight,
+  1,
+  100
 );
 const renderer = new THREE.WebGLRenderer();
-
-// scene.background = new THREE.Color(0xffffff);
-// renderer.setPixelRatio(devicePixelRatio);
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.BasicShadowMap
+renderer.shadowMap.type = THREE.BasicShadowMap;
 
 // CAM POSITION
-// cam.position.x = 0;
 cam.position.y = 17;
 cam.position.z = 22;
 
 document.body.appendChild(renderer.domElement);
-// const controls = new OrbitControls(cam, renderer.domElement);
 
 let controls = new PointerLockControls(cam, renderer.domElement);
 let clock = new THREE.Clock();
-
-
-let pointerElement = document.querySelector('#pointer');
-
-// let btn1 = document.querySelector('#button1');
-// btn1.addEventListener('click', () => {
-//     controls.lock();
-// })
-
-instructions.addEventListener('click', function () {
-
-    controls.lock();
-
+let pointerElement = document.querySelector("#pointer");
+instructions.addEventListener("click", function () {
+  controls.lock();
 });
 
-controls.addEventListener('lock', function () {
-
-    instructions.style.display = 'none';
-    blocker.style.display = 'none';
-
+controls.addEventListener("lock", function () {
+  instructions.style.display = "none";
+  blocker.style.display = "none";
 });
 
-controls.addEventListener('unlock', function () {
-
-    blocker.style.display = 'block';
-    instructions.style.display = '';
-
+controls.addEventListener("unlock", function () {
+  blocker.style.display = "block";
+  instructions.style.display = "";
 });
 
 let keyboardControls = [];
-addEventListener('keydown', (e) => {
-    keyboardControls[e.key] = true;
+addEventListener("keydown", (e) => {
+  keyboardControls[e.key] = true;
 });
-addEventListener('keyup', (e) => {
-    keyboardControls[e.key] = false;
+addEventListener("keyup", (e) => {
+  keyboardControls[e.key] = false;
 });
-
-function updatePointerPosition() {
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-
-    // Hitung posisi tengah layar
-    const centerX = screenWidth / 2;
-    const centerY = screenHeight / 2;
-
-    // Atur posisi pointer sesuai dengan posisi tengah layar
-    pointerElement.style.left = centerX + 'px';
-    pointerElement.style.top = centerY + 'px';
-}
-
-document.addEventListener('mousemove', (e) => updatePointerPosition(e));
 
 function processKeyboard(delta) {
-    let speed = 12;
-    let actualSpeed = speed * delta
-    if (keyboardControls['w']) {
-        controls.moveForward(actualSpeed);
-    }
-    if (keyboardControls['s']) {
-        controls.moveForward(-actualSpeed);
-    }
-    if (keyboardControls['a']) {
-        controls.moveRight(-actualSpeed);
-    }
-    if (keyboardControls['d']) {
-        controls.moveRight(actualSpeed);
-    }
+  let speed = 12;
+  let actualSpeed = speed * delta;
+  if (keyboardControls["w"]) {
+    controls.moveForward(actualSpeed);
+  }
+  if (keyboardControls["s"]) {
+    controls.moveForward(-actualSpeed);
+  }
+  if (keyboardControls["a"]) {
+    controls.moveRight(-actualSpeed);
+  }
+  if (keyboardControls["d"]) {
+    controls.moveRight(actualSpeed);
+  }
 }
 
-// 3D MODEL
-var room = lib.modelLoader(
-    scene,
-    'asset/room/scene.gltf',
-    { x: 18, y: 18, z: 18 },
-    { x: Math.PI / 2, y: Math.PI, z: Math.PI / 2 },
-    { x: 9, y: 0, z: 33 },
-    cam,
-    false
-);
-
+// // 3D MODEL
+// var room = lib.modelLoader(
+//     scene,
+//     'asset/room/scene.gltf',
+//     { x: 18, y: 18, z: 18 },
+//     { x: Math.PI / 2, y: Math.PI, z: Math.PI / 2 },
+//     { x: 9, y: 0, z: 33 },
+//     cam,
+//     false
+// );
 
 // AUDIO
 // const listener = new THREE.AudioListener();
@@ -125,124 +89,132 @@ var room = lib.modelLoader(
 
 //SOFA
 // var sofa = lib.modelLoader(
-//     scene, 
-//     'asset/sofa/scene.gltf', 
+//     scene,
+//     'asset/sofa/scene.gltf',
 //     { x: 0.035, y: 0.035, z: 0.035 },
 //     { x: Math.PI/2, y: Math.PI, z: 0 },
 //     { x: -5, y: 0, z: 15 }
 // );
 
-
-// KEYBOARD
-var keyboardModel = lib.modelLoader(
-    scene,
-    'asset/keyboard/scene.gltf',
-    { x: 0.008, y: 0.008, z: 0.008 },
-    { x: Math.PI / 2, y: Math.PI, z: Math.PI },
-    { x: -5.4, y: 10.7, z: 3 },
-    controls,
-    cam,
-    true,
-    { x: -2.7, y: 13, z: 5 },
-    7,
-    -2.7,
-    15,
-    3
-);
+// // KEYBOARD
+// var keyboardModel = lib.modelLoader(
+//     scene,
+//     'asset/keyboard/scene.gltf',
+//     { x: 0.008, y: 0.008, z: 0.008 },
+//     { x: Math.PI / 2, y: Math.PI, z: Math.PI },
+//     { x: -5.4, y: 10.7, z: 3 },
+//     controls,
+//     cam,
+//     true,
+//     { x: -2.7, y: 13, z: 5 },
+//     7
+// );
 
 // MOUSE
+// var mouse = lib.modelLoader(
+//     scene,
+//     'asset/mouse/scene.gltf',
+//     { x: 0.002, y: 0.002, z: 0.002 },
+//     { x: Math.PI / 2, y: 0, z: Math.PI / 2 },
+//     { x: 4.2, y: 11.5, z: 3 },
+//     controls,
+//     cam,
+//     true,
+//     { x: 4.2, y: 12.5, z: 5 },
+//     7,
+//     4,
+//     15,
+//     3
+// );
 var mouse = lib.modelLoader(
-    scene,
-    'asset/mouse/scene.gltf',
-    { x: 0.002, y: 0.002, z: 0.002 },
-    { x: Math.PI / 2, y: 0, z: Math.PI / 2 },
-    { x: 4.2, y: 11.5, z: 3 },
-    controls,
-    cam,
-    true,
-    { x: 4.2, y: 12.5, z: 5 },
-    7,
-    4,
-    15,
-    3
+  scene,
+  "asset/mouse/scene.gltf",
+  "mouse",
+  { x: 0.002, y: 0.002, z: 0.002 },
+  { x: Math.PI / 2, y: 0, z: Math.PI / 2 },
+  {x: 4.2, y: 11.5, z: 3},
+  pointerElement,
+  cam,
+  true,
+  {x: 4.2, y: 12.5, z: 5},
+  "buttonMouse"
 );
 
-// MONITOR
-var monitor = lib.modelLoader(
-    scene,
-    'asset/monitor/scene.gltf',
-    { x: 1.2, y: 1, z: 1 },
-    { x: Math.PI / 2, y: 0, z: Math.PI },
-    { x: -3, y: 15.2, z: 0 },
-    controls,
-    cam,
-    true,
-    { x: -2.2, y: 14.5, z: 0.8 },
-    7,
-    10,
-    15,
-    2
-);
+// // MONITOR
+// var monitor = lib.modelLoader(
+//     scene,
+//     'asset/monitor/scene.gltf',
+//     { x: 1.2, y: 1, z: 1 },
+//     { x: Math.PI / 2, y: 0, z: Math.PI },
+//     { x: -3, y: 15.2, z: 0 },
+//     controls,
+//     cam,
+//     true,
+//     { x: -2.2, y: 14.5, z: 0.8 },
+//     7,
+//     10,
+//     15,
+//     2
+// );
 
-// PC
-var pc = lib.modelLoader(
-    scene,
-    'asset/pc/scene.gltf',
-    { x: 0.78, y: 0.78, z: 0.78 },
-    { x: Math.PI / 2, y: Math.PI, z: Math.PI },
-    { x: 10, y: 15, z: 1.4 },
-    controls,
-    cam,
-    true,
-    { x: 10, y: 14.5, z: 0.8 },
-    7,
-    3,
-    15,
-    2
-);
+// // PC
+// var pc = lib.modelLoader(
+//     scene,
+//     'asset/pc/scene.gltf',
+//     { x: 0.78, y: 0.78, z: 0.78 },
+//     { x: Math.PI / 2, y: Math.PI, z: Math.PI },
+//     { x: 10, y: 15, z: 1.4 },
+//     controls,
+//     cam,
+//     true,
+//     { x: 10, y: 14.5, z: 0.8 },
+//     7,
+//     3,
+//     15,
+//     2
+// );
 
-// MOUSEPAD
-var mousepad = lib.modelLoader(
-    scene,
-    'asset/mousepad/scene.gltf',
-    { x: 0.7, y: 0.7, z: 0.7 },
-    { x: Math.PI / 2, y: Math.PI, z: Math.PI },
-    { x: -2, y: 10.2, z: 1.7 },
-    cam,
-    false,
-);
+// // MOUSEPAD
+// var mousepad = lib.modelLoader(
+//     scene,
+//     'asset/mousepad/scene.gltf',
+//     { x: 0.7, y: 0.7, z: 0.7 },
+//     { x: Math.PI / 2, y: Math.PI, z: Math.PI },
+//     { x: -2, y: 10.2, z: 1.7 },
+//     cam,
+//     false,
+// );
 
-// TABLE
-var table = lib.modelLoader(
-    scene,
-    'asset/table/scene.gltf',
-    { x: 8, y: 8, z: 8 },
-    { x: Math.PI / 2, y: Math.PI, z: Math.PI },
-    { x: 0, y: 0, z: 0 },
-    cam,
-    false,
-);
+// // TABLE
+// var table = lib.modelLoader(
+//     scene,
+//     'asset/table/scene.gltf',
+//     { x: 8, y: 8, z: 8 },
+//     { x: Math.PI / 2, y: Math.PI, z: Math.PI },
+//     { x: 0, y: 0, z: 0 },
+//     cam,
+//     false,
+// );
 
-// CHAIR
-var chair = lib.modelLoader(
-    scene,
-    'asset/chair/scene.gltf',
-    { x: 0.035, y: 0.035, z: 0.035 },
-    { x: Math.PI / 2, y: Math.PI, z: 0 },
-    { x: -3.4, y: 0, z: 15 },
-    cam,
-    false,
-);
+// // CHAIR
+// var chair = lib.modelLoader(
+//     scene,
+//     'asset/chair/scene.gltf',
+//     { x: 0.035, y: 0.035, z: 0.035 },
+//     { x: Math.PI / 2, y: Math.PI, z: 0 },
+//     { x: -3.4, y: 0, z: 15 },
+//     cam,
+//     false,
+// );
 
 // PLANE
 const plane = new THREE.PlaneGeometry(1000, 1000, 500, 500);
 const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
 const planeMesh = new THREE.Mesh(plane, planeMaterial);
-planeMesh.receiveShadow = true
+planeMesh.receiveShadow = true;
 planeMesh.position.set(-10, -1, 0);
 planeMesh.rotation.x = -Math.PI / 2;
 scene.add(planeMesh);
-
 
 // LIGHTING
 var ambient = new THREE.AmbientLight(0xffffff, 0.02);
@@ -269,15 +241,13 @@ spotlight.target.position.set(3, 1, 0);
 spotlight.castShadow = true;
 scene.add(spotlight);
 
-
 // SHOW MODEL
 function animate() {
-    requestAnimationFrame(animate);
-    // controls.update();
-    updatePointerPosition()
-    let delta = clock.getDelta();
-    processKeyboard(delta);
-    renderer.render(scene, cam);
+  requestAnimationFrame(animate);
+  // controls.update();
+  let delta = clock.getDelta();
+  processKeyboard(delta);
+  renderer.render(scene, cam);
 }
 
 animate();
--- model_loader.js
+++ model_loader.js
@@ -1,116 +1,188 @@
 // model_loader.js
 import * as THREE from "three";
 import { GLTFLoader } from "./node_modules/three/examples/jsm/loaders/GLTFLoader.js";
-import { Raycaster } from "three";
-import { TweenMax, Power2 } from "./node_modules/gsap/gsap-core.js";
 
 export function modelLoader(
-    scene,
-    filePath,
-    scale,
-    rotation,
-    position,
-    zoomin,
-    cam,
-    createClickable = true,
-    circlePosition,
-    zoomFactor, zoomX, zoomY, zoomZ
+  scene,
+  filePath,
+  name,
+  scale,
+  rotation,
+  position,
+  pointerElement,
+  cam,
+  createClickable = true,
+  circlePosition,
+  bulatObject
 ) {
-    const loader = new GLTFLoader();
-    const raycaster = new Raycaster();
-    const mouse = new THREE.Vector2();
+  const loader = new GLTFLoader();
+  let rayCast = new THREE.Raycaster();
+  let mouse = {};
+  let selected;
+  let arrow = new THREE.ArrowHelper(
+    rayCast.ray.direction,
+    cam.position,
+    50,
+    0xff0000
+  );
+  scene.add(arrow);
 
-    // Initialize clickableObjects array
-    raycaster.clickableObjects = [];
+  loader.load(filePath, (gltf) => {
+    var loader = gltf.scene.children[0];
+    loader.scale.set(scale.x, scale.y, scale.z);
+    loader.rotation.set(rotation.x, rotation.y, rotation.z);
+    loader.position.set(position.x, position.y, position.z);
+    loader.receiveShadow = true;
+    loader.castShadow = true;
+    loader.name = name;
+    scene.add(gltf.scene);
+  });
 
-    loader.load(filePath, (gltf) => {
-        var loader = gltf.scene.children[0];
-        loader.scale.set(scale.x, scale.y, scale.z);
-        loader.rotation.set(rotation.x, rotation.y, rotation.z);
-        loader.position.set(position.x, position.y, position.z);
-        loader.receiveShadow = true;
-        loader.castShadow = true;
-        scene.add(gltf.scene);
+  if (createClickable) {
+    // Object bulat
+    const clickableCircle = new THREE.Mesh(
+      new THREE.CircleGeometry(0.4, 32),
+      new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        opacity: 0.8,
+        transparent: true,
+      })
+    );
+    // clickableCircle.rotation.set(crotation.x, crotation.y, crotation.z); ==> buat putar object bulat
+    clickableCircle.position.copy(circlePosition || position);
+    clickableCircle.userData.clickable = true;
+    clickableCircle.name = bulatObject;
+    scene.add(clickableCircle);
+  }
 
-        // Check if a clickable circle should be created
-        if (createClickable) {
-            // Create clickable circle
-            const clickableCircle = new THREE.Mesh(
-                new THREE.CircleGeometry(0.4, 32),
-                new THREE.MeshBasicMaterial({
-                    color: 0xffffff,
-                    opacity: 0.8,
-                    transparent: true,
-                })
-            );
-            // clickableCircle.rotation.set(crotation.x, crotation.y, crotation.z);
-            clickableCircle.position.copy(circlePosition || position);
-            clickableCircle.userData.clickable = true;
-            scene.add(clickableCircle);
+  addEventListener("mousedown", (e) => {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = (e.clientY / window.innerHeight) * -2 + 1;
+    // console.log(mouse);
 
-            // Add to clickableObjects array
-            raycaster.clickableObjects.push(clickableCircle);
-        }
+    const pointerX = window.innerWidth / 2;
+    const pointerY = window.innerHeight / 2;
+
+    pointerElement.style.left = pointerX + "px";
+    pointerElement.style.top = pointerY + "px";
+
+    rayCast.setFromCamera(mouse, cam);
+    let items = rayCast.intersectObjects(scene.children);
+    arrow.setDirection(rayCast.ray.direction);
+
+    items.forEach((i) => {
+      if (i.object.name != "") {
+        console.log(i.object.name);
+        selected = i.object;
+      }
     });
+  });
 
-    // Add raycasting logic
-    window.addEventListener("click", onClick);
-
-    function onClick(event) {
-        // Calculate mouse position in normalized device coordinates
-        mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-        mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-        // Ensure the camera is an instance of THREE.PerspectiveCamera
-        if (!(cam instanceof THREE.PerspectiveCamera)) {
-            console.error("Invalid camera type. Please use a PerspectiveCamera.");
-            return;
-        }
-
-        // Update the picking ray with the camera and mouse position
-        raycaster.setFromCamera(mouse, cam);
-
-        // Calculate objects intersecting the picking ray
-        const intersects = raycaster.intersectObjects(raycaster.clickableObjects);
-
-        // if (intersects.length > 0) {
-        //   const clickedObject = intersects[0].object;
-        //   handleButtonClick(zoomFactor, zoomX, zoomY, zoomZ);
-        // }
-
-        handleButtonClick(zoomin, cam, zoomFactor, zoomX, zoomY, zoomZ);
-    }
+  if (selected == "buttonMouse") {
+    // ZOOM ANIMATION KE MOUSE
+  }
 }
 
-// Function to handle button click
-function handleButtonClick(zoomin, cam, zoomFactor, zoomX, zoomY, zoomZ) {
-    // Zoom in to the clicked object
-    // const targetPosition = clickedObject.position.clone().add(new THREE.Vector3(0, 0, zoomFactor || 5));
-    // const targetPosition = new THREE.Vector3(
-    //   clickedObject.position.x + zoomX,
-    //   clickedObject.position.y + zoomY,
-    //   clickedObject.position.z + (zoomFactor)
-    // );
-    // TweenMax.to(cam.position, 1, {
-    //   x: targetPosition.x,
-    //   y: targetPosition.y,
-    //   z: targetPosition.z,
-    //   ease: Power2.easeInOut,
-    // });
-    const targetX = zoomX;
-    const targetY = zoomY;
-    const targetZ = zoomZ;
+// export function modelLoader(
+//   scene,
+//   filePath,
+//   scale,
+//   rotation,
+//   position,
+//   zoomin,
+//   cam,
+//   createClickable = true,
+//   circlePosition,
+//   zoomFactor,
+//   zoomX,
+//   zoomY,
+//   zoomZ
+// ) {
+//   const loader = new GLTFLoader();
+//   const raycaster = new Raycaster();
+//   const mouse = new THREE.Vector2();
 
-    const zoom = zoomFactor;
-    const newPosition = new THREE.Vector3(targetX, targetY, targetZ + zoom);
+//   // Initialize clickableObjects array
+//   raycaster.clickableObjects = [];
 
-    cam.position.copy(newPosition);
+//   loader.load(filePath, (gltf) => {
+//     var loader = gltf.scene.children[0];
+//     loader.scale.set(scale.x, scale.y, scale.z);
+//     loader.rotation.set(rotation.x, rotation.y, rotation.z);
+//     loader.position.set(position.x, position.y, position.z);
+//     loader.receiveShadow = true;
+//     loader.castShadow = true;
+//     scene.add(gltf.scene);
 
-    // Make the camera look at the specified coordinates
-    const targetPosition = new THREE.Vector3(targetX, targetY, targetZ);
-    cam.lookAt(targetPosition);
+//     // Check if a clickable circle should be created
+//     if (createClickable) {
+//       // Create clickable circle
+//       const clickableCircle = new THREE.Mesh(
+//         new THREE.CircleGeometry(0.4, 32),
+//         new THREE.MeshBasicMaterial({
+//           color: 0xffffff,
+//           opacity: 0.8,
+//           transparent: true,
+//         })
+//       );
+//       // clickableCircle.rotation.set(crotation.x, crotation.y, crotation.z);
+//       clickableCircle.position.copy(circlePosition || position);
+//       clickableCircle.userData.clickable = true;
+//       scene.add(clickableCircle);
 
-    // Update the controls if necessary
-    zoomin.target.copy(targetPosition);
-    zoomin.update();
-}
+//       // Add to clickableObjects array
+//       raycaster.clickableObjects.push(clickableCircle);
+//     }
+//   });
+
+//   // Add raycasting logic
+//   window.addEventListener("click", onClick);
+
+//   function onClick(event) {
+//     // Calculate mouse position in normalized device coordinates
+//     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+//     mouse.y = -(event.clientY / window.innerHeight) * -2 + 1;
+
+//     // Ensure the camera is an instance of THREE.PerspectiveCamera
+//     if (!(cam instanceof THREE.PerspectiveCamera)) {
+//       console.error("Invalid camera type. Please use a PerspectiveCamera.");
+//       return;
+//     }
+
+//     // Update the picking ray with the camera and mouse position
+//     raycaster.setFromCamera(mouse, cam);
+
+//     // Calculate objects intersecting the picking ray
+//     const intersects = raycaster.intersectObjects(raycaster.clickableObjects);
+
+//     // if (intersects.length > 0) {
+//     //     const targetPosition = intersects[0].point;
+//     //     handleButtonClick(zoomin, cam, zoomFactor, targetPosition);
+//     //   }
+
+//     handleButtonClick(zoomin, cam, zoomFactor, zoomX, zoomY, zoomZ);
+//   }
+// }
+
+// // Function to handle button click
+// function handleButtonClick(zoomin, cam, zoomFactor, zoomX, zoomY, zoomZ) {
+//     // https://chat.openai.com/share/a88a3597-c6e9-4d2c-b45b-3b6027cc3b3a
+//     // https://chat.openai.com/share/55471b1f-f008-4133-8672-e2e5aa1a956e
+//   // Zoom in to the clicked object
+//   const targetX = zoomX;
+//   const targetY = zoomY;
+//   const targetZ = zoomZ;
+
+//   const zoom = zoomFactor;
+//   const newPosition = new THREE.Vector3(targetX, targetY, targetZ + zoom);
+
+//   cam.position.copy(newPosition);
+
+//   // Make the camera look at the specified coordinates
+//   const targetPosition = new THREE.Vector3(targetX, targetY, targetZ);
+//   cam.lookAt(targetPosition);
+
+//   // Update the controls if necessary
+//   zoomin.target.copy(targetPosition);
+//   zoomin.update();
+// }
